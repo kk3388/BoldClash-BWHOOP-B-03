863036372f8410cff5f881e3f999555358f2d706
silverag123 <arminio_grgic@yahoo.com>
2018-02-13 17:54:18 +0100

    Dual TLM&PIDs + PIDs on Devo screen

--- Silverware/src/config.h
+++ Silverware/src/config.h
@@ -196,9 +196,11 @@
 // external buzzer - pins in hardware.h
 //#define BUZZER_ENABLE
 
+
 // activate dual PIDs mode (silverAG)
 //#define ENABLE_DUAL_PIDS
 //#define PID_SET_CHANGE DEVO_CHAN_10 //channel used to switch between PID sets
+
 
 // Comment out to disable pid tuning gestures
 #define PID_GESTURE_TUNING
