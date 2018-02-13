28a8d5bb3be53cc0c8c0e67add255c918b9a1590
silverag123 <arminio_grgic@yahoo.com>
2018-02-13 18:09:08 +0100

    Save both PID sets (for dual PIDs mode)

--- Silverware/src/flash.c
+++ Silverware/src/flash.c
@@ -1,4 +1,3 @@
-
 #include "project.h"
 #include "drv_fmc.h"
 
@@ -8,6 +7,7 @@ extern void fmc_lock(void);
 
 extern float accelcal[];
 extern float * pids_array[3];
+extern float * pids_array2[3]; // dual PIDs code
 
 extern float hardcoded_pid_identifier;
 
@@ -15,7 +15,9 @@ extern float hardcoded_pid_identifier;
 #define FMC_HEADER 0x12AA0001
 
 float initial_pid_identifier = -10;
+float initial_pid_identifier2 = -10; // dual PIDs code
 float saved_pid_identifier;
+float saved_pid_identifier2; // dual PIDs code
 
 
 float flash_get_hard_coded_pid_identifier( void) {
@@ -29,12 +31,30 @@ float flash_get_hard_coded_pid_identifier( void) {
 	return result;
 }
 
+// ----- DUAL PIDS CODE ---------
+float flash_get_hard_coded_pid_identifier2( void) {
+	float result = 0;
+
+	for (int i=0;  i<3 ; i++) {
+		for (int j=0; j<3 ; j++) {
+			result += pids_array2[i][j] * (i+1) * (j+1) * 0.932f;
+		}
+	}
+	return result;
+}
+// ------- END OF DUAL PIDS CODE ---------
 
 void flash_hard_coded_pid_identifier( void)
 {
  initial_pid_identifier = flash_get_hard_coded_pid_identifier();
 }
 
+// ----- DUAL PIDS CODE ---------
+void flash_hard_coded_pid_identifier2( void)
+{
+ initial_pid_identifier2 = flash_get_hard_coded_pid_identifier2();
+}
+// ------- END OF DUAL PIDS CODE ---------
 
 
 
@@ -54,12 +74,22 @@ void flash_save( void) {
             fmc_write_float(addresscount++, pids_array[i][j]);
 		}
 	}
- 
 
     fmc_write_float(addresscount++, accelcal[0]);
     fmc_write_float(addresscount++, accelcal[1]);
     fmc_write_float(addresscount++, accelcal[2]);
 
+// ------- DUAL PIDS CODE ---------
+ 	fmc_write_float(addresscount++, initial_pid_identifier2 );
+	
+	for (int i=0;  i<3 ; i++) {
+		for (int j=0; j<3 ; j++) {
+            fmc_write_float(addresscount++, pids_array2[i][j]);
+		}
+	}
+// ------- END OF DUAL PIDS CODE ------------
+ 
+	
     writeword(255, FMC_HEADER);
     
 	fmc_lock();
@@ -86,12 +116,30 @@ void flash_load( void) {
      }
      else{
          addresscount+=9; 
-     }    
+     }
+
 
     accelcal[0] = fmc_read_float(addresscount++ );
     accelcal[1] = fmc_read_float(addresscount++ );
     accelcal[2] = fmc_read_float(addresscount++ );  
 
+// ------- DUAL PIDS CODE ----------
+	saved_pid_identifier2 = fmc_read_float(addresscount++);
+     if (  saved_pid_identifier2 == initial_pid_identifier2 )
+     {
+         for (int i=0;  i<3 ; i++) {
+            for (int j=0; j<3 ; j++) {
+                pids_array2[i][j] = fmc_read_float(addresscount++);
+            }
+        }
+     }
+/*     else{
+         addresscount+=9;  // not needed because there is no data after this, but in case something is added later, need to be used
+     }    */
+// ------- END OF DUAL PIDS CODE -------
+	
+	
+	
     }
     else
     {
@@ -99,16 +147,3 @@ void flash_load( void) {
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
